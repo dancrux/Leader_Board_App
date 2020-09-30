d1411044a5e3e6a3a95c919a591db6aff3b7d82f
dancrux <nwogwugwudaniel@gmail.com>
2020-09-30 14:26:48 +0100

    worked on Submit Fragment UI

--- app/src/main/java/com/cruxrepublic/leader/ui/TopLearnersFragment.kt
+++ app/src/main/java/com/cruxrepublic/leader/ui/TopLearnersFragment.kt
@@ -13,6 +13,7 @@ import com.cruxrepublic.leader.R
 import com.cruxrepublic.leader.api.RetrofitClient
 import com.cruxrepublic.leader.api.model.TopLearnersResponse
 import com.cruxrepublic.leader.ui.adapters.TopLearnersAdapter
+import kotlinx.android.synthetic.main.learner_item.*
 import kotlinx.android.synthetic.main.top_learners_fragment.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -38,10 +39,10 @@ class TopLearnersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fetchData()
         learnersRecycler.adapter = learnerAdapter
         learnersRecycler.layoutManager = LinearLayoutManager(this.activity)
 
-        fetchData()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -57,21 +58,29 @@ class TopLearnersFragment : Fragment() {
         call.enqueue(object : Callback<List<TopLearnersResponse>> {
             override fun onResponse(
                 call: Call<List<TopLearnersResponse>>,
-                response: Response<List<TopLearnersResponse>>) {
-                Toast.makeText(requireActivity(), "Error code is ${response.code()}", Toast.LENGTH_SHORT).show()
-                if (response.code() == 501) {
-                    val learners = response.body()!!
-                  learnerProgressBar.visibility = View.GONE
-                  learnerAdapter.addItems(learners)
+                response: Response<List<TopLearnersResponse>>
+            ) {
+
+                if (response.code() == 200) {
+//                    val learners = response.body()!!
+                    learnerProgressBar.visibility = View.GONE
+//                  learnerAdapter.addItems(response.body())
+                    response.body()?.let { learnerAdapter.addItems(it) }
                     Log.d(TAG, "${response.code()}")
 
                 } else {
+                    Toast.makeText(requireActivity(), "Error code is ${response.code()}",
+                        Toast.LENGTH_SHORT).show()
                     learnerProgressBar.visibility = View.GONE
                     learnersOfflineViews.visibility = View.VISIBLE
                 }
             }
 
             override fun onFailure(call: Call<List<TopLearnersResponse>>, t: Throwable) {
+                Toast.makeText(requireContext(), t.message, Toast.LENGTH_SHORT).show()
+//                nameTextView.text = t.message
+//                hoursTextView.text = t.message
+//                countryTextView.text = t.message
                 learnerProgressBar.visibility = View.GONE
                 learnersOfflineViews.visibility = View.VISIBLE
             }
